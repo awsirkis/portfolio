2ccfb0df2f906dd4348b8d2755ce2e86bf93187a
awsirkis <awsirkis@gmail.com>
2021-05-28 18:57:30 -0700

    Update index.vue

--- pages/nuxt/index.vue
+++ pages/nuxt/index.vue
@@ -7,6 +7,8 @@
       <pi SRC="cmaa_dragon.png" TITLE="Cascade Martial Arts Academy Website (2019 - )" DESCRIPTION="Main Page for a martial arts dojo. Technologies Featured: Databse Querying (PostgreSQL), Email (nodemailer), Blogging (marked), Authentication (custom shcheme),Role-based Access Control, Rule-Based Access Control, Calendar, Online Store" LNK="/nuxt/cmaa" />
       <pi SRC="icon.png" TITLE="Doc Storage Website (2021)" DESCRIPTION="Document Storage page originally developed for Ideal Facility. Technologies Featured: Database Querying (Sequelize/PostgreSQL), Email (nodemailer), Authentication, Role-based Access Control" LNK="/nuxt/docs" />
       <pi SRC="icon.png" TITLE="Ideal Facility Main Page" DESCRIPTION="Main Page for Ideal Critical Facility Services. I was asked to update the information and layout." LNK="/nuxt/if" />
+      <pi SRC="" TITLE="Straight to the Point Archery (Class 2019)" DESCRIPTION="Page created for class during web developer certification." LNK="https://awsirkis.github.io/archery/" />
+      <pi SRC="" TITLE="Adopt-a-Dog (Class 2019)" DESCRIPTION="Page created for class during web developer certification." LNK="https://awsirkis.github.io/adopt-a-dog-spring2019/" />
     </div>
   </article>
 </div>
